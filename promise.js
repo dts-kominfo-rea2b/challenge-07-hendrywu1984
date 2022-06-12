cfc04ca2c99a128d1cde13dc930dc47620332d76
Hendry Gunawan <hendry_wu1984@yahoo.com>
2022-06-12 22:44:38 +0700

    Hendry Gunawan 152235865101-334 done

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaction) => {
+  try {
+    let temp = 0;
+    temp = await promiseTheaterIXX()
+      .then((result) => countReaction(result, reaction))
+    temp += await promiseTheaterVGC()
+      .then((result) => countReaction(result, reaction))
+
+    return temp;
+  } catch (rejectMessage) {
+    console.log(rejectMessage);
+  }
+};
+
+const countReaction = (data, reaction) => {
+  return new Promise((resolve, reject) => {
+    if (data == "" || data == undefined) {
+      reject("Data not found!");
+    } else {
+      let counter = 0;
+
+      for (const datum of data) {
+        if (datum.hasil === reaction) counter++;
+      }
+
+      resolve(counter);
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
